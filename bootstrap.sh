04f20b5fa890a53f235172ee30570fa46c5cc4d2
professorbee <bee@tilde.team>
2019-03-19 21:23:01 -0400

    Fix on Mac

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
-echo "\e[32mLinking: \e[0mkakrc"
+echo "\033[32mLinking: \033[0mkakrc"
 sleep 1
 rm $HOME/.config/kak/kakrc
 ln -s $HOME/dotfiles/kakoune/kakrc $HOME/.config/kak/kakrc
-echo "\e[32mLinking: \e[0murls"
+echo "\033[32mLinking: \033[0murls"
 sleep 1
 rm $HOME/.newsboat/urls
 ln -s $HOME/dotfiles/newsboat/urls $HOME/.newsboat/urls
