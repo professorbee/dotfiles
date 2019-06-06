35bbf77879afb1799cf90fde04935c81b852ac4d
professorbee <bee@tilde.team>
2019-06-06 16:05:56 -0400

    Update script to include newsboat config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,9 @@
-echo "\033[32mLinking: \033[0mkakrc"
-sleep 1
+printf "\033[32mLinking: \033[0mkakrc\n"
 rm $HOME/.config/kak/kakrc
 ln -s $HOME/dotfiles/kakoune/kakrc $HOME/.config/kak/kakrc
-echo "\033[32mLinking: \033[0murls"
-sleep 1
+printf "\033[32mLinking: \033[0murls\n"
 rm $HOME/.newsboat/urls
 ln -s $HOME/dotfiles/newsboat/urls $HOME/.newsboat/urls
+printf "\033[32mLinking: \033[0mnewsboat\n"
+rm $HOME/.newsboat/config
+ln -s $HOME/dotfiles/newsboat/config $HOME/.newsboat/config
